62c11e13770e0e1db85255812c25ce41a696f448
rdkvks <rdkv95@gmail.com>
2025-03-29 13:00:52 +0300

    initial commit 2

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,10 @@
 # This file is intentionally left blank.
+# It is used to mark the directory as a Python package.
+# The __init__.py file can be empty or contain package initialization code.
+# In this case, it is empty to keep the package structure clean.
+# The presence of this file allows Python to recognize the directory as a package,
+
+# enabling the import of modules from this directory in other parts of the codebase.
+# This is useful for organizing related modules and functions into a cohesive package.
+# The package can be imported in other scripts or modules using the package name.
+# For example, if this package is named 'telegram_chat_summarizer', it can be imported as:
